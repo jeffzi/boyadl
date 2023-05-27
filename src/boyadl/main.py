f53a8633287f04a426d04ff293d434b08355eb0b
Jean-Francois Zinque <16199817+jeffzi@users.noreply.github.com>
2023-05-27 16:48:31 +0200

    feat: add -o argument

--- src/boyadl/main.py
+++ src/boyadl/main.py
@@ -2,31 +2,46 @@
 import json
 import re
 from io import BytesIO
+from pathlib import Path
 from urllib.request import urlopen
 
 import typer
 from pydub import AudioSegment
+from typing_extensions import Annotated
 
 
-app = typer.Typer()
+app = typer.Typer(
+    help="Download mp3 files for Boya Chinese books", add_completion=False
+)
+cwd = Path(".")
 
-def _download_audio(lesson_name:str, url: str) -> None:
+
+def _download_audio(url: str, filepath: Path) -> None:
     buffer = BytesIO(urlopen(url).read())
     buffer.seek(0)
     AudioSegment.from_file(buffer).export(
-        f"{lesson_name}.mp3", format="mp3", parameters=["-q:a", "0"]
+        filepath, format="mp3", parameters=["-q:a", "0"]
     )
 
+
 @app.command()  # type: ignore
-def main(url: str) -> None:
-    """Download mp3 files for Boya Chinese books.
-
-    Args:
-        url (str): The url found after scanning the QR code on the back cover of the book.
-
-    Raises:
-        Exit: If an error occurred.
-    """
+def main(
+    url: str,
+    output: Annotated[
+        Path,
+        typer.Option(
+            "--output",
+            "-o",
+            exists=True,
+            file_okay=False,
+            dir_okay=True,
+            writable=True,
+            resolve_path=True,
+            help="Output directory",
+        ),
+    ] = cwd,
+) -> None:
+    """Download mp3 files for Boya Chinese books."""
     content = urlopen(url).read().decode()
     pattern = re.compile(r"^var data\s?=\s?(\{.*play_url.*http.*\})", re.MULTILINE)
     raw_data = pattern.findall(content)
@@ -34,7 +49,7 @@ def main(url: str) -> None:
     if not raw_data:
         typer.echo("List of lessons not found, did you pass the appropriate url?")
         raise typer.Exit(1)
-    
+
     if len(raw_data) > 1:
         typer.echo("Ambiguous list of lessons, did you pass the appropriate url?")
         raise typer.Exit(1)
@@ -45,7 +60,7 @@ def main(url: str) -> None:
         for lesson in data["tree_list"]
     }
     for lesson_name, url in lessons.items():
-        _download_audio(lesson_name, url)
+        _download_audio(url, output / f"{lesson_name}.mp3")
 
 
 if __name__ == "__main__":
